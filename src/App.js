738751522888da9c99913b8fc0139e8955e0a2c4
aabiro <aaryn.alexander@gmail.com>
2017-12-29 18:45:34 -0500

    implemented two way data binding and css styling

--- src/App.js
+++ src/App.js
@@ -13,13 +13,23 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     //console.log('Was clicked');
     //DO NOT DO THIS this.state.persons[0].name = 'Maaaaaan';
     this.setState( {
       persons: [
-        {name: 'Maxamillion', age: 48},
-        {name: 'Aaryn', age: 19},
+        {name: newName, age: 48},
+        {name: 'Aaryn B', age: 19},
+        {name: 'Steph', age:35}
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        {name: 'Max', age: 48},
+        {name: event.target.value, age: 19},
         {name: 'Steph', age:35}
       ]
     })
@@ -30,11 +40,23 @@ class App extends Component {
       <div className="App">
         <h1>Hi!!!</h1>
         <p>This is working.</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}  />
-        <Person name="Aaryn" age='28'>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}  />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}  />
+        <button onClick={() => this.switchNameHandler('Maxamillion')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}  />
+        <Person
+          name="Aaryn"
+          age='28'
+          //bind syntax is better for performance
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          >My Hobbies: Racing</Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}/>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}  />
       </div>
     );
     //return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hi, I\'m a React App!!'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -7,14 +7,17 @@
 
 import React from 'react';
 
+//use these function type of components as much as possible, most simple
+// components usually do not hold or manipulate state
 const person = (props) => {
     //return some JSX -looks like HTML, with some dynamic Javascript content
     //return <p>Im a Person and Im {Math.floor(Math.random() * 30)} years old</p>
     //dynamic content set from outside tags
     return (
       <div>
-          <p>Im {props.name} and Im {props.age} years old</p>
+          <p onClick={props.click}>Im {props.name} and Im {props.age} years old</p>
           <p>{props.children}</p>
+          <input type='text' onChange={props.changed} value={props.name} />
       </div>)
 };
 
